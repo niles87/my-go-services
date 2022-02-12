b6a3b662a426e0d2062554c9bf9dc3e6fe0dfe98
Niles Bingham <nbing19@gmail.com>
2022-02-11 18:59:57 -0800

    Adding basic functionality to server

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/niles87/my-go-services
+
+go 1.17
+
+require (
+	github.com/gofiber/fiber/v2 v2.26.0
+	github.com/gofiber/template v1.6.22
+	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.33.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+)
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/gofiber/template/html"
+	"github.com/joho/godotenv"
+)
+
+type Message struct {
+	Msg string
+}
+
+func getIndex(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{
+		"Title":  "Welcome",
+		"Second": "To my services",
+	})
+}
+
+type User struct {
+	Name  string
+	Email string
+}
+
+func getUser(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(user)
+}
+
+var user User
+
+func createUser(c *fiber.Ctx) error {
+
+	body := new(User)
+
+	err := c.BodyParser(body)
+
+	if err != nil {
+		c.Status(fiber.StatusBadRequest).JSON(Message{Msg: err.Error()})
+		return err
+	}
+
+	user = User{
+		Name:  body.Name,
+		Email: body.Email,
+	}
+	return c.Status(fiber.StatusCreated).JSON(user)
+}
+
+func updateUser(c *fiber.Ctx) error {
+
+	body := new(User)
+	err := c.BodyParser(body)
+	if err != nil {
+		c.Status(fiber.StatusBadRequest).JSON(Message{Msg: err.Error()})
+		return err
+	}
+
+	user = *body
+	return c.Status(fiber.StatusAccepted).JSON(user)
+}
+
+func main() {
+	// Load dotenv file
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("ENV failed to load")
+	}
+
+	// Set port (for heroku later)
+	PORT := os.Getenv("PORT")
+
+	// use Golangs default html templates
+	engine := html.New("./views", ".html")
+
+	// Initialize app
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	// Display index route
+	app.Get("/", getIndex)
+
+	// Add middleware with .Use
+	app.Use(logger.New())
+	app.Use(requestid.New())
+
+	// Group related endpoints together
+	userApp := app.Group("/user")
+	userApp.Get("", getUser)
+	userApp.Post("", createUser)
+	userApp.Put("", updateUser)
+
+	log.Fatal(app.Listen(PORT))
+}
