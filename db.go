c42099f1e68dc42211048b1c7dee698658f5139c
Niles Bingham <nbing19@gmail.com>
2022-02-15 18:14:24 -0800

    added allow native passwords bool

--- db.go
+++ db.go
@@ -16,12 +16,14 @@ func connect() {
 	var err error
 	if prod != "" {
 		mysqlCfg := mysql.Config{
-			User:   os.Getenv("USER"),
-			Passwd: os.Getenv("PASSWORD"),
-			Net:    os.Getenv("NET"),
-			Addr:   os.Getenv("ADDRESS"),
-			DBName: os.Getenv("DB_NAME"),
+			User:                 os.Getenv("USER"),
+			Passwd:               os.Getenv("PASSWORD"),
+			Net:                  os.Getenv("NET"),
+			Addr:                 os.Getenv("ADDRESS"),
+			DBName:               os.Getenv("DB_NAME"),
+			AllowNativePasswords: true,
 		}
+		fmt.Println(mysqlCfg.FormatDSN())
 		db, err = sql.Open("mysql", mysqlCfg.FormatDSN())
 		if err != nil {
 			log.Fatal("Failed to connect", err)
